e66aab623012f8a57bd9a265cf9e662986afb111
noahlapolt <noahlapolt@gmail.com>
2022-04-14 21:50:00 -0400

    Basic Sound

---
+++ main.py
@@ -0,0 +1,30 @@
+import pyaudio
+
+if __name__ == '__main__':
+    CHUNK = 1024
+    WIDTH = 2
+    CHANNELS = 2
+    RATE = 44100
+    RECORD_SECONDS = 10
+
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(WIDTH),
+                channels=CHANNELS,
+                rate=RATE,
+                input=True,
+                output=True,
+                frames_per_buffer=CHUNK)
+
+    print("* recording")
+
+    for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+        data = stream.read(CHUNK)  #read audio stream
+        stream.write(data, CHUNK)  #play back audio stream
+
+    print("* done")
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
